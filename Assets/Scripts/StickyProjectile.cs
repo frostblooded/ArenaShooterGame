13fc653ab22355d87b651a7746240eea59a62a24
Nikolay Danailov <frostblooded@yahoo.com>
2021-02-01 15:33:00 +0200

    Add sounds and particles on hat wall hit

--- Assets/Scripts/StickyProjectile.cs
+++ Assets/Scripts/StickyProjectile.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class StickyProjectile : Projectile
 {
+    public AudioSource hitAudioSource;
+    public ParticleSystem hitParticleSystem;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
+            hitAudioSource.Play();
+            hitParticleSystem.Play();
             GetComponent<PlagueHat>().enabled = true;
             Destroy(GetComponent<AudioSource>());
             Destroy(this);
